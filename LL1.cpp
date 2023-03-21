cb2c76811266e1be3b4544720f756df20a4d5b61
AnubhavSaxena3 <113455210+AnubhavSaxena3@users.noreply.github.com>
2023-03-21 16:04:13 +0530

    updated

--- LL1.cpp
+++ LL1.cpp
@@ -103,24 +103,103 @@ node *reverse(node *&head)
     return prevptr;
 }
 
-node* reverseRecursive(node* &head)
+node *reverseK(node *&head, int k)
+{
+    node *prevptr = NULL;
+    node *currptr = head;
+    node *nextptr;
+    int count = 0;
+    while (currptr != NULL && count < k)
+    {
+        nextptr = currptr->next;
+        currptr->next = prevptr;
+        prevptr = currptr;
+        currptr = nextptr;
+        count++;
+    }
+    if (nextptr != NULL)
+    {
+        head->next = reverseK(nextptr, k);
+    }
+    return prevptr;
+}
+
+node *reverseRecursive(node *&head)
 {
     if (head == NULL || head->next == NULL)
     {
         return head;
     }
-    node* newhead = reverseRecursive(head->next);
-    
+    node *newhead = reverseRecursive(head->next);
+
     head->next->next = head;
     head->next = NULL;
     return newhead;
 }
+
+// create and remove cycle
+void makeCycle(node *&head, int pos)
+{
+    node *temp = head;
+    node *startNode;
+    int count = 1;
+    while (temp->next != NULL)
+    {
+        if (count == pos)
+        {
+            startNode = temp;
+        }
+        temp = temp->next;
+        count++;
+    }
+    temp->next = startNode;
+}
+
+bool detectCycle(node *&head)
+{
+    node *slow = head;
+    node *fast = head;
+
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (fast == slow)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+// for remove cycle
+void removeCycle(node *&head)
+{
+    node *slow = head;
+    node *fast = head;
+    do
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    } while (slow != fast);
+
+    fast = head;
+    while (slow->next != fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next;
+    }
+    slow->next = NULL;
+}
 int main()
 {
-    node* head = NULL;
+    node *head = NULL;
     insertAtTail(head, 1);
     insertAtTail(head, 2);
     insertAtTail(head, 3);
+    insertAtTail(head, 4);
+    insertAtTail(head, 5);
+    insertAtTail(head, 6);
     display(head);
     insertAtHead(head, 4);
     display(head);
@@ -130,9 +209,15 @@ int main()
     // deleteAtHead(head);//only for deleting head element
     display(head);
     // node* newhead = reverse(head);
-
-    
-    node* newhead = reverseRecursive(head);
+    // node *newhead = reverseRecursive(head);
+    // display(newhead);
+    int k = 2;
+    node *newhead = reverseK(head, k);
     display(newhead);
-    
+    makeCycle(head, 3); // this recall that cycle present in our ll
+    // display(head);//this loops to the cycle
+    cout << detectCycle(head) << endl;
+    removeCycle(head);
+    cout << detectCycle(head) << endl;
+    display(head);
 }
